94327784d9aa8cae77a3050fea6225188c66be69
Jake <contact.jakerieger@gmail.com>
2023-04-15 15:53:38 -0400

    merge dev

---
+++ lunar/include/types.h
@@ -0,0 +1,19 @@
+// Copyright © 2023 Gymate, Inc.
+//
+// Licensed under the GNU General Public License v3.0
+
+#ifndef LUNA_TYPES_H
+#define LUNA_TYPES_H
+
+#define BOOL_TRUE 0x1
+#define BOOL_FALSE 0x0
+
+typedef double Number;
+typedef const char* String;
+#pragma pack(1)
+typedef struct Boolean {
+    unsigned value : 1;
+} Boolean;
+#pragma pack(0)
+
+#endif  //LUNA_TYPES_H
